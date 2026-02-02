5ce28c83d17cf4abe18327a3d2f2f61dfcf22667
AhmedSM11 <ahmedalmasnoer@gmail.com>
2026-02-02 17:11:05 +0300

    add sonarQube analysis

---
+++ scan-all.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+SONAR_URL="http://localhost:9000"
+ADMIN_AUTH="admin:admin"
+# List your project folders here
+PROJECTS=("products" "authorization")
+
+echo "Waiting for SonarQube to be ready..."
+until $(curl --output /dev/null --silent --head --fail $SONAR_URL); do
+    printf '.'
+    sleep 5
+done
+echo -e "\nSonarQube is UP."
+
+
+for DIR in "${PROJECTS[@]}"; do
+    echo ">>> Analyzing Project: $DIR"
+    cd "$DIR" || exit
+    chmod +x mvnw
+    
+    # 1. Create Project
+    curl -u $ADMIN_AUTH -X POST "$SONAR_URL/api/projects/create?name=$DIR&project=$DIR"
+    
+    # 2. Generate Token
+    TOKEN=$(curl -u $ADMIN_AUTH -X POST "$SONAR_URL/api/user_tokens/generate?name=token-$DIR-$(date +%s)" | grep -o '"token":"[^"]*' | grep -o '[^"]*$')
+
+    # 3. Run Scan (All on one line to prevent "No such file" errors)
+    ./mvnw clean verify sonar:sonar -Dsonar.projectKey="$DIR" -Dsonar.host.url="$SONAR_URL" -Dsonar.login="$TOKEN" -Dsonar.coverage.jacoco.xmlReportPaths=target/site/jacoco/jacoco.xml
+
+    cd ..
+done
+
+echo "All projects scanned! View them at $SONAR_URL"
